08f50b496ff7055c8eb9bc305238a30e7b93f40d
Jacob Goodall <jgoodall@opp.local>
2018-07-09 12:06:25 -0500

    styled form

--- client/src/components/Contact/ContactForm.js
+++ client/src/components/Contact/ContactForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import '../../styles/Contact.css'
+
 class ContactForm extends Component {
 
     state = {
@@ -50,25 +52,53 @@ class ContactForm extends Component {
           });
         alert('Your message has been sent. :-)')
         this.setState({name: '', company: '', email: '', phone: '', message: ''})
-        return null;
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
     }
 
     render(){
         console.log(this.state)
         return (
-            <div className="container">
+            <div className="container" style={{marginTop: '60px', marginBottom: '60px'}}>
                 <div className="row">
                     <div className="col-md-4">
-                        address
+                    <div className="address">
+                            <h4 className="overture-contact-address">Address</h4>
+                            <h5 className="info-contact">595 Lakeview Parkway</h5>
+                            <h5 className="infoBreak-contact">Vernon Hills, IL 60061</h5>
+                        </div>
+                        <div className="phone">
+                            <h4 className="overture-contact-address" >Phone</h4>
+                            <h5 className="info-contact">P. 888.756.9564</h5>
+                            <h5 className="info-contact">F. 847.680.0114</h5>
+                        </div>
+                        <div className="socials-contact">
+                            <a href="" target="blank"><i className="fa fa-facebook social-contact"></i></a>
+                            <a href="" target="blank"><i className="fa fa-instagram social-contact"></i></a>
+                            <a href="" target="blank"><i className="fa fa-twitter social-contact"></i></a>
+                            <a href="" target="blank"><i className="fa fa-youtube social-contact"></i></a>
+                        </div>
                     </div>
                     <div className="col-md-8">
-                        <form onSubmit={this.handleSubmit}> 
-                            <input type='text' name="name" placeholder="Name" value={this.state.name} onChange={this.handleChange} />
-                            <input type='text' name="company" placeholder="Company" value={this.state.company} onChange={this.handleChange} />
-                            <input type='text' name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} />
-                            <input type='text' name="phone" placeholder="Phone" value={this.state.phone} onChange={this.handleChange} />
-                            <input type='text' name="message" placeholder="Message" value={this.state.message} onChange={this.handleChange} />
-                            <button>Send</button>
+                        <form onSubmit={this.handleSubmit} className="contact-form row">
+                            <div className="col-xs-6 contact-inputs-box">
+                                <input className="contact-inputs" type='text' name="name" placeholder="Name*" required value={this.state.name} onChange={this.handleChange} />
+                            </div>
+                            <div className="col-xs-6 contact-inputs-box">
+                                <input className="contact-inputs" type='text' name="company" placeholder="Company" value={this.state.company} onChange={this.handleChange} />
+                            </div> 
+                            <div className="col-xs-6 contact-inputs-box">
+                                <input className="contact-inputs" type='email' name="email" placeholder="Email*" required value={this.state.email} onChange={this.handleChange} />
+                            </div> 
+                            <div className="col-xs-6 contact-inputs-box">
+                                <input className="contact-inputs" type='text' name="phone" placeholder="Phone" value={this.state.phone} onChange={this.handleChange} />
+                            </div>  
+                            <div className="col-xs-12 contact-inputs-box">
+                                <textarea className="contact-inputs text-area-contact" type='text' name="message" required placeholder="Message*" value={this.state.message} onChange={this.handleChange} />
+                            </div>
+                            <div className="col-xs-12" style={{textAlign: 'right'}}>
+                                <button className="contact-button-send">Send</button>
+                            </div>
                         </form>
                     </div>
                 </div>
