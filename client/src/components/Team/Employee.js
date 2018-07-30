8793f79f5c765586b3734fa650885ceb8382ae6e
Jacob Goodall <jgoodall@opp.local>
2018-07-30 15:32:54 -0500

    changed rule

--- client/src/components/Team/Employee.js
+++ client/src/components/Team/Employee.js
@@ -9,7 +9,9 @@ const Employee = (props) => {
             <img src={props.image} alt="headshot" className="employee-headshot" />
             <h3 className="employee-name">{props.name}</h3>
             {/* <div style={{height: '1.5px', width: '20%', backgroundColor: '#ef7521'}}></div> */}
-            <img src={Rule} alt="ruler" style={{width: '50px'}} />
+            <div  style={{width: '50px'}}>
+                <img src={Rule} alt="ruler" style={{height: '4px', width: '100%'}} />
+            </div>
             <h4 className="employee-title">{props.title}</h4>
         </div>
     )
