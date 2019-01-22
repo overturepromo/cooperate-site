b4846bf8a41e70a19d30f47ec2e305449d87a82f
Jacob Goodall <jgoodall@opp.local>
2019-01-22 16:10:52 -0600

    trying outlook

--- functions.js
+++ functions.js
@@ -1,13 +1,11 @@
 var nodemailer = require('nodemailer');
 
 exports.contactEmail = (name, company, email, phone, message) => {
-    var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      secure: true,
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
-      },
+  var transporter = nodemailer.createTransport({
+      host: 'mail.overturepromo.com',
+      port: 25,
+      secure:false,
+      ignoreTLS:true
     });
     var mailOptions = {
       from: email,
@@ -26,12 +24,10 @@ exports.contactEmail = (name, company, email, phone, message) => {
 
   exports.applyEmail = (position, first, last, email, phone, movies, coverLetter, resume) => {
     var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      secure: true,
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-      },
+      host: 'mail.overturepromo.com',
+      port: 25,
+      secure:false,
+      ignoreTLS:true
     });
     var mailOptions = {
       from: email,
@@ -56,30 +52,26 @@ exports.contactEmail = (name, company, email, phone, message) => {
     });
   };
 
-//   const transporter = nodemailer.createTransport({
-    // host: 'smtp.ethereal.email',
-    // port: 587,
-    // auth: {
-    //     user: 'l4ulcdjrietnchxt@ethereal.email',
-    //     pass: 'vKRrRj9Jdj63BsUR5c'
-    // }
-// });
-
-
-  // service: 'gmail',
-  // auth: {
-  //   user: auth.USERNAME,
-  //   pass: auth.PASSWORD
-  // }
-
-  // host: 'mail.overturepromo.com',
-  // port: 25,
-  // secure: false,
-  // ignoreTLS: true
-
-  // attachments: [
-  //   {   // file on disk as an attachment
-  //     filename: 'beer.jpg',
-  //     path: './beer.jpg' // stream this file
-  //   },
-  // ]  
+  // exports.contactEmail = (name, company, email, phone, message) => {
+  //   var transporter = nodemailer.createTransport({
+  //     service: 'gmail',
+  //     secure: true,
+  //     auth: {
+  //       user: process.env.MAIL_USER,
+  //       pass: process.env.MAIL_PASS
+  //     },
+  //   });
+  //   var mailOptions = {
+  //     from: email,
+  //     to: 'jacobg@overturepromo.com', //hr@overturepromo.com 
+  //     subject: 'You have a new contact request',
+  //     html: `<h4>name:</h4> ${name} <h4>company:</h4> ${company} <h4>email:</h4> ${email} <h4>phone:</h4> ${phone} <h4>message:</h4> ${message}`,
+  //   };
+  //   transporter.sendMail(mailOptions, function (error, info) {
+  //     if (error) {
+  //       console.log('error is ' + error);
+  //     } else {
+  //       console.log('Email sent: ' + info.response);
+  //     }
+  //   });
+  // };
