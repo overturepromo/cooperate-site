5fd69e59b8e8218e66b7fd1e3ca393b1112ff56b
Jacob Goodall <jgoodall@opp.local>
2019-02-11 15:18:45 -0600

    trying new key

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 const uuidv4 = require('uuid/v4');
 const fn = require("./functions.js");
 const knox = require('knox');
-const awsS3Url = "https://s3.amazonaws.com/overture-corporate-site-resumes/";
+const awsS3Url = "https://s3.amazonaws.com/overtures-resumes/";
 const fs = require('fs');
 var enforce = require('express-sslify');
 
@@ -38,7 +38,7 @@ const upload = multer({
 const client = knox.createClient({
   key: process.env.AWS_KEY,
   secret: process.env.AWS_SECRET,
-  bucket: 'overture-corporate-site-resumes'
+  bucket: 'overtures-resumes'
 });
 
 
