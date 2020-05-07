0051d9104fbd9cbfbde8272cb8be96aa8d2d0896
Jacob Goodall <jgoodall@opp.local>
2020-05-07 15:52:00 -0500

    add me to emails

--- functions.js
+++ functions.js
@@ -16,7 +16,7 @@ var mail = require('nodemailer').mail;
       });
     var mailOptions = {
       from: email,
-      to: 'media@overturepromo.com', //hr@overturepromo.com 
+      to: 'media@overturepromo.com, jacobg@overturepromo.com', //hr@overturepromo.com 
       subject: 'You have a new contact request',
       html: `<h4>name:</h4> ${name} <h4>company:</h4> ${company} <h4>email:</h4> ${email} <h4>phone:</h4> ${phone} <h4>message:</h4> ${message}`,
     };
@@ -92,99 +92,3 @@ var mail = require('nodemailer').mail;
       }
     });
   };
-
-
-  // THIS IS THE OAUTH INFO
-
-  // auth: {
-  //   type: 'OAuth2',
-  //   user: process.env.OAUTH_USER,
-  //   clientId: process.env.OAUTH_CLIENTID,
-  //   clientSecret: process.env.OAUTH_CLIENTSECRET,
-  //   refreshToken: process.env.OAUTH_REFRESHTOKEN,
-  //   accessToken: process.env.OAUTH_ACCESSTOKEN,
-  // },
-
-
-
-  // ORIGINALS!!
-
-  // exports.contactEmail = (name, company, email, phone, message) => {
-  //   var transporter = nodemailer.createTransport({
-  //         service: 'gmail',
-  //         secure: true,
-  //         auth: {
-  //           user: process.env.MAIL_USER,
-  //           pass: process.env.MAIL_PASS
-  //         },
-  //     });
-  //     var mailOptions = {
-  //       from: email,
-  //       to: 'media@overturepromo.com, jacobg@overturepromo.com', //hr@overturepromo.com 
-  //       subject: 'You have a new contact request',
-  //       html: `<h4>name:</h4> ${name} <h4>company:</h4> ${company} <h4>email:</h4> ${email} <h4>phone:</h4> ${phone} <h4>message:</h4> ${message}`,
-  //     };
-  //     transporter.sendMail(mailOptions, function (error, info) {
-  //       if (error) {
-  //         console.log('error is ' + error);
-  //       } else {
-  //         console.log('Email sent: ' + info.response);
-  //       }
-  //     });
-  //   };
-
-  // exports.applyEmail = (position, first, last, email, phone, movies, coverLetter, resume) => {
-  //   var transporter = nodemailer.createTransport({
-  //       service: 'gmail',
-  //       secure: true,
-  //       auth: {
-  //         user: process.env.MAIL_USER,
-  //         pass: process.env.MAIL_PASS
-  //       },
-  //   });
-  //   var mailOptions = {
-  //     from: email,
-  //     to: 'jacobg@overturepromo.com', //hr@overturepromo.com
-  //     subject: `You have a new application for ${position}`,
-  //     html: `<h4>First:</h4> ${first} 
-  //            <h4>Last:</h4> ${last}
-  //            <h4>Email:</h4> ${email} 
-  //            <h4>Phone:</h4> ${phone} 
-  //            <h4>Movies:</h4> ${movies}
-  //            <h4>Cover Letter</h4> ${coverLetter} `,
-  //     attachments: [{
-  //       href: resume
-  //     }]
-  //   };
-  //   transporter.sendMail(mailOptions, function (error, info) {
-  //     if (error) {
-  //       console.log('error is ' + error);
-  //     } else {
-  //       console.log('Email sent: ' + info.response);
-  //     }
-  //   });
-  // };
-
-  // USING OVERTURES OUTLOOK
-
-  // exports.contactEmail = (name, company, email, phone, message) => {
-  // var transporter = nodemailer.createTransport({
-  //     host: 'mail.overturepromo.com',
-  //     port: 587,
-  //     secure: true,
-  //     ignoreTLS: false
-  //   });
-  //   var mailOptions = {
-  //     from: email,
-  //     to: 'jacobg@overturepromo.com', //hr@overturepromo.com 
-  //     subject: 'You have a new contact request',
-  //     html: `<h4>name:</h4> ${name} <h4>company:</h4> ${company} <h4>email:</h4> ${email} <h4>phone:</h4> ${phone} <h4>message:</h4> ${message}`,
-  //   };
-  //   transporter.sendMail(mailOptions, function (error, info) {
-  //     if (error) {
-  //       console.log('error is ' + error);
-  //     } else {
-  //       console.log('Email sent: ' + info.response);
-  //     }
-  //   });
-  // };
