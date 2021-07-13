02f5f4e1236e5751123498d452f4e5763eb1ce19
jacobg@overturepromo.com <jacobg@overturepromo.com>
2021-07-12 20:05:31 -0500

    adding new job

--- client/src/components/Careers/Positions/ACRiverNorth.js
+++ client/src/components/Careers/Positions/ACRiverNorth.js
@@ -23,28 +23,26 @@ class ACRiverNorth extends Component {
     render(){
         return (
             <div>
-                <p style={{color: 'gray'}}><strong>Summary: </strong>Your primary responsibility in this role is to handle all aspects of customer account orders, including order processing, product quotes and coordinating product sample requests – in partnership with our vendors. This role is all about customer relationships and using those relationships, while assisting the Sr. Account Manager with support of their business. The expectation is that this person will quickly become an expert with our offered products and associated suppliers by educating customers and helping them to make promotional marketing decisions.</p>
+                <p style={{color: 'gray'}}><strong>Summary: </strong>Our Account Manager, Inside Sales & eCommerce is an entry level sales role; no cold-calling is required. It is eCommerce-based selling, with customers coming to our website to order promotional products, and communicating with account managers through email and web chat. The ideal candidate will have a background in customer service and/or inside sales, and have average mathematical skills to accurately provide quotes and pricing. Your primary responsibility in this role is to handle all aspects of customer account orders, including order processing, product quotes and coordinating product sample requests – in partnership with our vendors. Growth in this role is about establishing strong customer relationships through excellent service, and using those relationships to build your own book of business. The expectation is that you will, with training, become an expert in our offered products and suppliers, and will be able to help customers choose promotional products that meet their branding and budget requirements.</p>
                 <h4>PRIMARY DUTIES AND RESPONSIBILITIES</h4>
                 <ul style={{color: "gray"}}>
                     <li>Handle all aspects of customer account orders, including order processing, product quotes and coordinating product sample requests, with our clients as well as our vendors</li>
                     <li>Expand responsibilities by learning and sourcing trending products to be able to fully support clients</li>
                     <li>Assist new and existing customers with inquires while providing consultative support and recommendations on promotional products</li>
-                    <li>Proactively communicating with existing customers to build and maintain the customer relationship</li>
-                    <li>By becoming an expert with our offered products and their associated suppliers, you will educate customers on those products and help them make promotional marketing decisions.</li>
-                    <li>Identify and evaluate opportunities to increase customer retention and satisfaction by developing a positive rapport and relationship with customers</li>
-                    <li>Act as an overall coordinator and partner with other departments including warehouse, fulfillment, production and accounting</li>
-                    <li>Provide day to day support to Sr Account Manager and assist and handle ad hoc tasks as required</li>
+                    <li>Proactively communicate with existing customers to build and maintain the relationships and expand your book of business</li>
+                    <li>Leverage internally provided training and opportunities to meet with suppliers, such that you will be able to discuss products and help customers make promotional marketing decisions.</li>
+                    <li>Coordinate orders with other departments including warehouse, fulfillment, production and accounting.</li>
                 </ul>
                 <h4>YOU ARE THE PERFECT PERSON IF YOU HAVE</h4>
                 <ul style={{color: "gray"}}>
                     <li>Previous customer service or sales experience</li>
-                    <li>Above average communication skills</li>
+                    <li>Good communication skills</li>
                     <li>Must be able to prioritize and handle multiple tasks in a changing work environment</li>
                     <li>Average mathematical skills to accurately provide quotes and pricing</li>
                     <li>Be creative and up to speed on industry trends and new and trendy products</li>
                     <li>Must be motivated, driven and have the ambition to succeed</li>
                     <li>Have a positive, upbeat and team player attitude</li>
-                    <li>Must have the ability and be comfortable to work independently</li>
+                    <li>Must have the ability to work independently</li>
                     <li>Must have strong attention to detail</li>
                     <li>MS Office proficiency in Word, Excel, PowerPoint and Outlook</li>
                 </ul>
@@ -55,7 +53,7 @@ class ACRiverNorth extends Component {
                 <button className="apply-button" onClick={this.openModal}>APPLY FOR POSITION</button>
                 <h5 onClick={() => this.props.bye('acRiverNorth')} className="view-button">Hide Details</h5>
                 <Modal isOpen={this.state.showModal} onRequestClose={this.closeModal} className="apply-lightbox">
-                    <ApplyBox title="Account Manager" close={this.closeModal} />
+                    <ApplyBox title="Account Manager, Inside Sales" close={this.closeModal} />
                 </Modal>
             </div>
         )
