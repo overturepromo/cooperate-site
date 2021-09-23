18f83981a70b4f0839f8c789e2de66eff6dc4145
jacobg@overturepromo.com <jacobg@overturepromo.com>
2021-09-23 16:09:46 -0500

    billing coordinator

--- client/src/components/Careers/Positions/APClerk.js
+++ client/src/components/Careers/Positions/APClerk.js
@@ -24,16 +24,20 @@ class APClerk extends Component {
         return (
             <div>
                 <h4>Job Description</h4>
-                <p style={{color: 'gray'}}>Are you a self-starter, master communicator, and Excel whiz?  If you are looking to build your career in Finance/Accounting, this may be the role for you!  With general direction, this role performs a variety of general accounting functions to ensure invoices are processed in a timely manner.  This role also includes the following responsibilities:</p>
+                <p style={{color: 'gray'}}>Are you a self-starter, master communicator, and Excel whiz?  If you are looking to build your career in Finance/Accounting, this may be the role for you!  With general direction, this role performs a variety of billing functions to ensure the preparation of accurate and timely bills to our customers.  This also includes the following responsibilities:</p>
                 <h4> Responsibilities </h4>
                 <ul style={{color: "gray"}}>
-                    <li>Receive, verify and process incoming invoices and credits within our sales and accounting platform, NetSuite</li>
-                    <li>Reconcile any discrepancies with invoices in a timely fashion</li>
-                    <li>Comfortability with communicating to any employee in the company, especially when inquiring about invoices and/or payments</li>
-                    <li>Responsible for keeping updated spreadsheets of inventory costs to be allocated</li>
-                    <li>Review statements frequently to stay on top of any missing invoices</li>
-                    <li>Research and resolve routine and non-routine vouching questions and discrepancies</li>
-                    <li>Communication directly with vendors on discrepancies, payment requests and invoice questions, all while providing excellent customer service</li>
+                    <li>Gather and apply merchandise and shipping costs to orders</li>
+                    <li>Invoice customer orders in a timely manner</li>
+                    <li>Analyze open order reports and communicate directly with sales teams</li>
+                    <li>Willing to work with all departments of the company whenever necessary</li>
+                    <li>Run, organize, and distribute various reports to sales teams and monthly reporting to clients</li>
+                    <li>Check accuracy of orders and prepare invoices for billing</li>
+                    <li>Develop up-to-date knowledge of various client billing requirements</li>
+                    <li>Research and resolve routine and non-routine billing questions and discrepancies</li>
+                    <li>Prepare credit memos for returns or adjustments</li>
+                    <li>Answer questions from customers or from the Accounts Receivable department</li>
+                    <li>Communicate directly with clients and provide excellent customer service</li>
                     <li>Organizing and entering vendor bills as needed</li>
                     <li>Performs other duties as assigned</li>
                 </ul>
@@ -58,15 +62,18 @@ class APClerk extends Component {
                     <li>401(k) eligibility day one of employment, with a Company match after the first year of employment</li>
                     <li>Paid maternity and paternity leave based on years of service</li>
                     <li>Relaxed dress code</li>
+                    <li>Overture family fun – Summer BBQs, volunteer engagement groups (Culture Crew, Green Team, Safety Team, Diversity and Inclusion Advisory Council), employee vegetable garden, contests, potlucks and cookoffs</li>
+                    <li>Employee referral bonus</li>
                 </ul>
                 <h4>Education/Experience</h4>
                 <ul style={{color: "gray"}}>
-                    <li>High school diploma or equivalent experience required, Associates degree preferred.</li>
+                    <li>BS/BA degree or equivalent combination of education and experience required.</li>
                 </ul>
+                <p>*Relocation is not provided for this role.</p>
                 <button className="apply-button" onClick={this.openModal}>APPLY FOR POSITION</button>
                 <h5 onClick={() => this.props.bye('apClerk')} className="view-button">Hide Details</h5>
                 <Modal isOpen={this.state.showModal} onRequestClose={this.closeModal} className="apply-lightbox">
-                    <ApplyBox title="Vouching Coordinator" close={this.closeModal} />
+                    <ApplyBox title="Billing Coordinator" close={this.closeModal} />
                 </Modal>
             </div>
         )
