983868e0a0e0c105d50332896b3cb635add9c30d
jacobg@overturepromo.com <jacobg@overturepromo.com>
2021-11-10 18:50:54 -0600

    graphic design update again

--- client/src/components/Careers/Positions/GraphicDesginer.js
+++ client/src/components/Careers/Positions/GraphicDesginer.js
@@ -24,34 +24,27 @@ class GraphicDesigner extends Component {
         return (
             <div>
                 <h4>SUMMARY</h4>
-                <p style={{color: 'gray'}}>Overture has an opening for a talented, creative Graphic Designer in a fast paced, fun environment that encourages creativity.  You will work on a wide range of print and digital design projects, all of which require an innovative and detail-oriented approach.  This role offers a good balance of projects in which you will use clients’ artwork and prepare it for printing in addition to being able to create your own, custom artwork.</p>
-                <p style={{color: 'gray'}}>Our ideal candidate must have a working knowledge of Adobe Photoshop, Illustrator and InDesign. Our perfect candidate will have knowledge of preparing artwork for print materials, screen-printing, embroidery, promotional items, and websites. Must be a team player, but can work independently; is personable, friendly and can provide excellent customer service to both internal and external clients.  </p>
+                <p style={{color: 'gray'}}>Overture is looking for a mid-level Graphic Designer to work on a wide range of print and digital design projects. We are looking for someone with an innovative and detail-oriented approach. In this role, you will be able to work with a variety of well-known brands, help to develop high-visibility promo projects, and be able to create your own custom artwork.</p>
+                <p style={{color: 'gray'}}>Our ideal candidate must have a working knowledge of Adobe Photoshop, Illustrator, InDesign and prototyping tools for website creation. Our perfect candidate will have knowledge of preparing artwork for print materials, screen-printing, embroidery, and promotional items. Web design experience is required. Must have experience working in a team environment, but can work independently. Can provide excellent customer service to both internal and external clients.</p>
                 <h4>Essential Duties and Responsibilities </h4>
                 <ul style={{color: "gray"}}>
                     <li>Ability to work in a fast-paced environment and manage multiple projects with tight deadlines</li>
                     <li>Develop graphics for products, websites, eBlasts, collateral, logos/branding, and more</li>
-                    <li>Marketing: create blogs, eblasts, and images for presentation decks</li>
-                    <li>Convert raster artwork into vector artwork </li>
-                    <li>Produce virtual product mockups </li>
-                    <li>Product photography</li>
                     <li>Design promotional webstores and present them to clients</li>
+                    <li>Marketing: create blogs, eblasts, and images for presentation decks</li>
                     <li>Communicate effectively with internal sales teams and managers, including providing updates about projects and deadlines</li>
-                    <li>Maintain composure in stressful situations</li>
                     <li>Work with our copywriter to develop social media content and messaging for other graphics</li>
                     <li>Participate in our team critique of creative work and work collaboratively with the team</li>
                 </ul>
                 <h4>Qualifications</h4>
                 <ul style={{color: "gray"}}>
                     <li>Proficient with Adobe Illustrator, Photoshop, and InDesign</li>
-                    <li>Knowledge of typography, hierarchy, color theory, layout, and using a grid</li>
                     <li>Web design experience preferred (coding skills not required, but you must have an understanding of responsive/mobile design and best practices for web design)</li>
                     <li>UXPin or other prototyping tool experience</li>
                     <li>Proven resourcefulness with creative problem-solving skills</li>
                     <li>Excellent communication skills</li>
-                    <li>Detail-oriented</li>
                     <li>Positive, upbeat and team player attitude</li>
                     <li>Animation and After Effects experience a plus</li>
-                    <li>Video experience a plus</li>
                 </ul>
                 <h4>Benefits</h4>
                 <ul style={{color: "gray"}}>
