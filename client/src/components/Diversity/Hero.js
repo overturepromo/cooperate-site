357464a2a427fa603ea3340de303fac066531c06
jacobg@overturepromo.com <jacobg@overturepromo.com>
2022-01-03 15:54:45 -0600

    linking decks

--- client/src/components/Diversity/Hero.js
+++ client/src/components/Diversity/Hero.js
@@ -8,6 +8,9 @@ const Hero = () => {
             <div className="diversity-hero-box container">
                 <p className="diversity-cta-top animated fadeInDown">DIVERSITY AT OVERTURE</p>
                 <p className="diversity-cta-bottom animated fadeInDown">Overture is minority-owned and woman-led. We value different views and perspectives to help us evolve. We work tirelessly to ensure Overture is a diverse, equitable, and inclusive workplace.</p>
+                <p className="diversity-cta-bottom animated fadeInDown">
+                    <a className="diversity-cta-bottom diverse-link animated fadeInDown" href="https://issuu.com/overturepromotions/docs/overture_diverse_suplier_products" target="blank">Check Out Our Favorite Diverse Supplier Products</a>
+                </p>
             </div>
         </div>
     )
--- client/src/components/Sustainability/Hero.js
+++ client/src/components/Sustainability/Hero.js
@@ -8,6 +8,9 @@ const Hero = () => {
             <div className="sus-hero-box container">
                 <p className="sus-cta-top animated fadeInDown">We Believe in Sustainable Business Practices</p>
                 <p className="sus-cta-bottom animated fadeInDown">Our future depends on how we live in the present. Overture is committed to year-over-year improvement in regards to ethical, sustainable, and eco-responsible business practices. We continually strive to minimize our environmental impact.</p>
+                <p className="diversity-cta-bottom animated fadeInDown">
+                    <a className="diversity-cta-bottom diverse-link animated fadeInDown" href="https://issuu.com/overturepromotions/docs/overture_sustainability_deck" target="blank">Check Out Our Favorite Sustainable Products</a>
+                </p>
             </div>
         </div>
     )
