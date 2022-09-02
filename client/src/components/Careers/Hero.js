b7844d984102e9e4cfa815a80081c14647fa9db0
jacobg@overturepromo.com <jacobg@overturepromo.com>
2022-09-01 23:09:00 -0500

    updated career page

--- client/src/components/Careers/Hero.js
+++ client/src/components/Careers/Hero.js
@@ -7,7 +7,11 @@ const Hero = () => {
         <div className="hero-careers">
             <div className="career-hero-box container">
                 <p className="work-cta-top animated fadeInDown">CAREERS</p>
-                <p className="work-cta-bottom animated fadeInDown">We’re growing and we’re hiring. Rockstars only.</p>
+                <div>
+                    <a className='careers-button'>HOLD UP!</a>
+                </div>
+                <p className="careers-cta-bottom animated fadeInDown">Studies have shown that women and people of color are less likely to apply to jobs they don’t meet every single qualification for. At Overture, building a diverse workforce is a high priority and, let’s face it, we don’t want to miss out on someone great over a job requirement!</p>
+                <p className="careers-cta-bottom animated fadeInDown"> We encourage you to apply to any role you’re interested in, regardless of whether or not your past experience aligns with every qualification. Who knows? You could be one application away from your dream role!</p>
             </div>
         </div>
     )
