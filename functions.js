57a463778dbbe1b1b95eb2c2c4ee3bc8584dfba7
jacobg@overturepromo.com <jacobg@overturepromo.com>
2022-09-29 14:51:31 -0500

    apply email

--- functions.js
+++ functions.js
@@ -50,7 +50,7 @@ var mail = require('nodemailer').mail;
     });
     var mailOptions = {
       from: email,
-      to: 'christinan@overturepromo.com, jacobg@overturepromo.com, glendaD@overturepromo.com, AnnaN@overturepromo.com',
+      to: 'christinan@overturepromo.com, jacobg@overturepromo.com, AngelaK@overturepromo.com',
       subject: `You have a new application for ${position}`,
       html: `<h4>First:</h4> ${first} 
              <h4>Last:</h4> ${last}
