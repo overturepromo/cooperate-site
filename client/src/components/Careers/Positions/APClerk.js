2e0d4c741993d53a2311cfdf2f9188530f732609
jacobg@overturepromo.com <jacobg@overturepromo.com>
2023-01-04 15:24:20 -0600

    career change

--- client/src/components/Careers/Positions/APClerk.js
+++ client/src/components/Careers/Positions/APClerk.js
@@ -24,47 +24,56 @@ class APClerk extends Component {
         return (
             <div>
                 <h4>Summary</h4>
-                <p style={{color: 'gray'}}>At Overture, our people are our most important resource. We are currently looking for a highly skilled recruiter to become one of them, and to help us find them. The ideal candidate will have prior recruiting experience or an HR background with knowledge of all hiring and onboarding practices. The recruiter will report directly to the VP, Human Resources.</p>
+                <p style={{color: 'gray'}}>The primary responsibility of a Receiving Associates is to accurately receive all in-bound purchase orders. This requires carefully inspecting all boxes, crates, totes, and product for damage by comparing items received with accompanied documentation ensuring a match and noting any discrepancies. This position is also responsible for providing support in various areas of the warehouse according to business needs.  This is a full-time position Monday- Friday, 1st shift (8:00am-4:30pm).</p>
                 <h4>Duties and Responsibilities</h4>
                 <ul style={{color: "gray"}}>
-                    <li>Develops, facilitates, and implements all phases of the recruitment process in an effort to build a robust candidate pipeline</li>
-                    <li>Partner closely with HR team to build recruitment process, strategy and communications for the company. Including assisting in the selection and development of an ATS, job guides and interview guides for all company roles</li>
-                    <li>Collaborates with department managers to gain an understanding of the company’s hiring needs for each position, and identify accurate job descriptions and hiring criteria</li>
-                    <li>Identifies and implements efficient and effective recruiting methods and strategies based on the available role, industry standards, and the needs of the organization</li>
-                    <li>Assists with job posting and advertisement processes</li>
-                    <li>Screen resumes and prospects, qualify, interview and manage candidate flow throughout the process from initial interview through final offer and negotiation</li>
-                    <li>Performs reference checks and background screens as necessary</li>
-                    <li>Schedules interviews; oversees preparation of interview questions and other hiring and selection materials</li>
-                    <li>Assists with the interview process, attending and conducting interviews with managers, directors, and other stakeholders if needed</li>
-                    <li>Grow and foster high-touch relationships with a diverse candidate pool and maintain a pipeline of talent to pull from as new positions open up throughout the company</li>
-                    <li>Collaborates with the hiring manager and/or other human resource staff during the offer process, identifying and recommending salary ranges, incentives, start dates, and other pertinent details.</li>
-                    <li>Ensures compliance with federal, state, and local employment laws and regulations, and company policies during the entire process</li>
-                    <li>Attends and participates in college job fairs and recruiting sessions</li>
-                    <li>Stay active and up to speed on all current job posting sites, and partner with Marketing on posting to social networking platforms</li>
-                    <li>Stays up to speed on current and evolving recruiting trends and best practices</li>
-                    <li>Performs other duties as assigned</li>
-                </ul>
-                <h4>Skills and Competencies</h4>
-                <ul style={{color: "gray"}}>
-                    <li>Excellent verbal and written communication skills.</li>
-                    <li>Excellent interpersonal skills with good negotiation tactics.</li>
-                    <li>Ability to create and implement sourcing strategies for recruitment for a variety of roles.</li>
-                    <li>Proactive and independent with the ability to take initiative.</li>
-                    <li>Excellent time management skills with a proven ability to meet deadlines.</li>
-                    <li>Familiarity with laws, regulations, and best practices applicable to hiring and recruitment.</li>
-                    <li>Proficient with or the ability to quickly learn applicant-tracking software or other recruitment systems.</li>
-                    <li>Proficient with Microsoft Office Suite or related software.</li>
+                    <li>Receives material coming into the building by vendors, customers, or other warehouses accurately and promptly</li>
+                    <li>Unloads and stocks incoming merchandise, picking of outgoing merchandise, and loading of outbound trailers as needed</li>
+                    <li>Verifies the quantity and description of materials received by checking merchandise against packing lists</li>
+                    <li>Safely operate all the necessary equipment in your work area only when properly trained or certified to do so</li>
+                    <li>May be responsible for palletizing and shrink-wrapping items</li>
+                    <li>May be required to stack pallets and repeatedly lift up to 40 pounds</li>
+                    <li>Ability to work as a team player, as well as independently</li>
+                    <li>Reports damages and vendor compliance issues</li>
+                    <li>Maintains an acceptable level of attendance based on Company attendance policy</li>
+                    <li>Ensures safety techniques are used in all aspects of daily work responsibilities</li>
+                    <li>Maintain warehouse clean and free of debris at all times</li>
+                    <li>Ability to withstand unavoidable exposure to unfavorable atmospheric conditions or extreme temperatures inside and outside the facility, including noise, dust and non-toxic screen-printing fumes</li>
+                    <li>Other duties as assigned </li>
                 </ul>
                 <h4>Education/Experience</h4>
                 <ul style={{color: "gray"}}>
-                    <li>Bachelor’s degree in Human Resources or related field, or equivalent work experience</li>
-                    <li>Minimum of 2 years recruiting experience required</li>
+                    <li>High School Diploma or equivalent working experience</li>
+                    <li>Previous receiving experience preferred </li>
+                    <li>Must be able to multitask and work in a fast pace environment</li>
+                    <li>Able to follow instructions and production schedules </li>
+                    <li>Ability to work cohesively with team members and independently  </li>
+                    <li>Able to read and write in English</li>
+                    <li>RF scanner gun experience preferred </li>
+                    <li>Must be able to lift up to 40 lbs.</li>
+                    <li>Able to walk, stand, push, pull, reach, bend, stoop, climb, and carry 100% of the day</li>
+                    <li>Ability to determine priorities and accomplish multiple tasks with minimal supervision</li>
+                    <li>Must have reliable and dependable transportation</li>
+                    <li>Experience operating warehouse equipment: Cherry picker or Order picker, stand-up unit, sit-down regular fork-truck.</li>
+                    <li>Experience with WMS (warehouse management system)</li>
+                    <li>Must have basic computer skills, especially with Outlook</li>
                 </ul>
-                <p>*Relocation is not provided for this role.</p>
+                <h4>Benefits</h4>
+                <ul style={{color: "gray"}}>
+                    <li>Complete insurance coverage – Medical, dental, vision, life insurance, flex spending accounts and disability plans with Company contributions</li>
+                    <li>Pet insurance and bring your dog to work days</li>
+                    <li>Corporate partnerships with Life Time Fitness, Bears Fit, Skechers and Tickets at Work</li>
+                    <li>Generous paid time off (vacation, sick and personal days as well as yearly floating holiday)</li>
+                    <li>401(k) eligibility day one of employment, with a Company match after the first year of employment</li>
+                    <li>Paid maternity and paternity leave based on years of service</li>
+                    <li>Relaxed dress code</li>
+                    <li>Overture family fun – Summer BBQs, volunteer engagement groups (Culture Crew, Green Team, Safety Team, Diversity and Inclusion Advisory Council), employee vegetable garden, contests, potlucks and cookoffs</li>
+                    <li>Employee referral bonus</li>
+                </ul>
                 <button className="apply-button" onClick={this.openModal}>APPLY FOR POSITION</button>
                 <h5 onClick={() => this.props.bye('apClerk')} className="view-button">Hide Details</h5>
                 <Modal isOpen={this.state.showModal} onRequestClose={this.closeModal} className="apply-lightbox">
-                    <ApplyBox title="Recruiter" close={this.closeModal} />
+                    <ApplyBox title="Receiving Associate" close={this.closeModal} />
                 </Modal>
             </div>
         )
