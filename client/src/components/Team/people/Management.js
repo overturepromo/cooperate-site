f3456363672bd498b596539f7fb1f75c74ac4d71
jacobg@overturepromo.com <jacobg@overturepromo.com>
2023-01-30 15:38:34 -0600

    chris s

--- client/src/components/Team/people/Management.js
+++ client/src/components/Team/people/Management.js
@@ -178,7 +178,7 @@ export const People = {
     },
     ChrisS: {
         name: 'Chris Sheehy',
-        title: 'Director of Web Development',
+        title: 'CTO',
         bio: '',
         movie: '',
         vacation: '',
