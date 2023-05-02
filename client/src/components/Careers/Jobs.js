0c0f66d2e21ad55e0922ea7f244ab662a5873b85
jacobg@overturepromo.com <jacobg@overturepromo.com>
2023-05-02 15:37:36 -0500

    careers

--- client/src/components/Careers/Jobs.js
+++ client/src/components/Careers/Jobs.js
@@ -113,12 +113,12 @@ class Jobs extends Component {
                     <div style={{borderTop: '1px solid #d8dadc'}}>
                         <h3><a className="apply-link" target='blank' href="https://overturepromotions.applytojob.com/apply/Ss2cg9R6Xl/Account-Manager-Inside-Sales">Account Manager, Inside Sales</a></h3>
                     </div>
-                    <div style={{borderTop: '1px solid #d8dadc'}}>
+                    {/* <div style={{borderTop: '1px solid #d8dadc'}}>
                         <h3><a className="apply-link" target='blank' href="https://overturepromotions.applytojob.com/apply/0Uq651OEMJ/Controller">Controller</a></h3>
-                    </div>
-                    <div style={{borderTop: '1px solid #d8dadc'}}>
+                    </div> */}
+                    {/* <div style={{borderTop: '1px solid #d8dadc'}}>
                         <h3><a className="apply-link" target='blank' href="https://overturepromotions.applytojob.com/apply/EjRINVP1PS/Graphic-Designer">Graphic Designer</a></h3>
-                    </div>
+                    </div> */}
                     {/* <div style={{borderTop: '1px solid #d8dadc'}}>
                         <h3>Purchasing Manager</h3>
                         <h5 onClick={() => this.handleOn('acWaukegan')} className="view-button">View Details</h5>
