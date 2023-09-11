79b4f6d9081d4ea03e97c97e1c5be1e5fea13db1
Kevin Doss <kevind@overturepromo.com>
2023-09-11 16:11:50 -0500

    adding Customer Care Coordinator job per wrike: https://www.wrike.com/open.htm?id=1197879775

--- client/src/components/Careers/Jobs.js
+++ client/src/components/Careers/Jobs.js
@@ -116,6 +116,9 @@ class Jobs extends Component {
                     <div style={{borderTop: '1px solid #d8dadc'}}>
                         <h3><a className="apply-link" target='blank' href="https://overturepromotions.applytojob.com/apply/Ch1DdgBXQ2/Distribution-Center-Manager">Distribution Center Manager</a></h3>
                     </div>
+                    <div style={{borderTop: '1px solid #d8dadc'}}>
+                        <h3><a className="apply-link" target='blank' href="https://overturepromotions.applytojob.com/apply/BkdpNd7koj/Customer-Care-Coordinator">Customer Care Coordinator</a></h3>
+                    </div>
                     {/* <div style={{borderTop: '1px solid #d8dadc'}}>
                         <h3><a className="apply-link" target='blank' href="https://overturepromotions.applytojob.com/apply/JAYogz20tU/Billing-Coordinator">Billing Coordinator</a></h3>
                     </div> */}
