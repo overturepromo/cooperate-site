b74d511a631d2281e96c9f8052240991aa91c77a
Jacob Goodall <JacobG@overturepromo.com>
2024-02-07 14:27:44 -0600

    acqusition

--- client/src/components/Team/index.js
+++ client/src/components/Team/index.js
@@ -4,7 +4,6 @@ import {Helmet} from 'react-helmet';
 import Hero from './Hero'
 import JumpAround from './JumpAround';
 import Board from './Board';
-import Acquisition from './Acquisition';
 import Management from './Management';
 import Experts from './Experts';
 import Sales from './Sales';
@@ -38,7 +37,6 @@ const index = () => {
                 <Route path='/team/warehouse' component={Warehouse} />
                 <Route path='/team/experts' component={Experts} />
                 <Route path='/team/board' component={Board} />
-                <Route path='/team/acquisition' component={Acquisition} />
                 <Route path='/team/management' component={Management} />
                 <Route path='/team/devit' component={DevIT} />
                 <Route path='/team' component={Management} />                
