dc07f96874f6564ec5d0be562e5af937d221d5e2
Jacob Goodall <JacobG@overturepromo.com>
2024-03-06 14:48:54 -0600

    jobs

--- client/src/components/Careers/Jobs.js
+++ client/src/components/Careers/Jobs.js
@@ -68,6 +68,17 @@ class Jobs extends Component {
               </a>
             </h3>
           </div>
+          <div style={{ borderTop: "1px solid #d8dadc" }}>
+            <h3>
+              <a
+                className="apply-link"
+                target="blank"
+                href="https://overturepromotions.applytojob.com/apply/zcgavBTwGM/Purchasing-Assistant"
+              >
+                Purchasing Assistant
+              </a>
+            </h3>
+          </div>
 
           {/* <div style={{borderTop: '1px solid #d8dadc'}}>
                         <h3>Web Developer </h3>
